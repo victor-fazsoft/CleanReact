5130d5afa2c0eac78cc91adba61f9b7ea49a8f72
Victor Martins <victormartins9723@outlook.com>
2023-10-06 10:19:47 -0300

    feat: add authentication usecase

---
+++ src/a.spec.ts
@@ -0,0 +1,7 @@
+import { describe } from "node:test";
+
+describe('aa', () => {
+    test('aa', () => {
+        expect(1).toBe(1)
+    })
+})
---
+++ src/domain/usecases/authentication.ts
@@ -0,0 +1,10 @@
+import {AccountModel} from '../models/AccountModel'
+
+type AuthenticationParams = {
+    email: string,
+    passwork: string
+}
+
+export interface Authentication {
+    auth(params: AuthenticationParams): Promise<AccountModel>
+}
