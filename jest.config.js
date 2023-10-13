ac269c9c8ee3ddd12697578ae1a58342ada1ff50
Victor Martins <victormartins9723@outlook.com>
2023-10-13 15:36:38 -0300

    chore: config sass

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,8 @@
 module.exports = {
     roots: ['<rootDir>/src', '<rootDir>/src'],
     collectCoverageFrom: [
-      '<rootDir>/src/**/*.{ts,tsx}'
+      '<rootDir>/src/**/*.{ts,tsx}',
+      '!**/*.d.ts'
     ],
     coverageDirectory: 'coverage',
 
@@ -10,6 +11,7 @@ module.exports = {
       '.+\\.(ts|tsx)$': 'ts-jest'
     },
     moduleNameMapper: {
-      '@/(.*)': '<rootDir>/src/$1'
+      '@/(.*)': '<rootDir>/src/$1',
+      '\\.scss$': 'idendtity-obj-proxy'
     }
 }
