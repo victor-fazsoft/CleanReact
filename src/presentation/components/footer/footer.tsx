222411c254ff42509166b9b33fcc980a42de2a3f
Victor Martins <victormartins9723@outlook.com>
2023-10-16 13:12:03 -0300

    refactor: moving header and footer as component schema

---
+++ src/presentation/components/footer/footer.tsx
@@ -0,0 +1,11 @@
+import React, { memo } from 'react';
+import Styles from './footer-styles.scss';
+
+const Footer = () => {
+    return (
+        <footer className={Styles.footer}></footer>
+
+    );
+}
+
+export default memo(Footer);
