b8d41ea14626c281828ccee645631ffb2e1b306b
Victor Martins <victormartins9723@outlook.com>
2023-10-16 11:48:13 -0300

    refactor: move logo to a separate file

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Styles from './login-styles.scss'
 import Spinner from '@/presentation/components/spinner/spinner';
+import Logo from '@/presentation/components/logo/logo'
 
 const Login: React.FC = () => {
     return (
         <div className={Styles.login}>
             <header className={Styles.header}>
-                <img src="" alt="" />
+                <Logo />
                 <h1>4Dev - Enquetes para programadores</h1>
             </header>
             <form className={Styles.form} action="" >
