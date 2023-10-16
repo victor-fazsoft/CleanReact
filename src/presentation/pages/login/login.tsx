02092ed04fac0c3359c9ec47f236764462072211
Victor Martins <victormartins9723@outlook.com>
2023-10-16 11:41:21 -0300

    feat: add login ciew

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
 import Styles from './login-styles.scss'
+import Spinner from '@/presentation/components/spinner/spinner';
 
 const Login: React.FC = () => {
     return (
         <div className={Styles.login}>
-            Hello
+            <header className={Styles.header}>
+                <img src="" alt="" />
+                <h1>4Dev - Enquetes para programadores</h1>
+            </header>
+            <form className={Styles.form} action="" >
+                <h2>Login</h2>
+                <input type="email" name="email" placeholder="email..." />
+                <input type="password" name="password" placeholder="senha..." />
+                <button type='submit'>Entrar</button>
+                <span className={Styles.link}>Criar conta</span>
+                <div className={Styles.errorWrap}>
+                    <Spinner className={Styles.spinner}/>
+                    <span className={Styles.error}>Erro</span>
+                </div>
+            </form>
+            <footer className={Styles.footer}></footer>
         </div>
     )
 }
